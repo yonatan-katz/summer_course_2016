662c94a77ff2c59c9e460638f37251ce0dff9090
yonic <test@mint.(none)>
2016-07-03 18:17:08 +0300

    PCA regresiion almost done

--- netflix.py
+++ netflix.py
@@ -30,10 +30,10 @@ def load_data():
     
     x.columns = movies.ix[:, 1]    
     
-    return x, y, y_dates
+    return x, y, y_dates, movies
     
 def print_same_stat():    
-    x,y,dates = load_data()
+    x,y,dates,movies = load_data()
     
     g = x.iloc[:,range(14)]
     
@@ -45,10 +45,7 @@ def print_same_stat():
     
     print "Y vs Date corr:", np.corrcoef (y[0],dates[0])[0][1]
     
-    
-def regresion_test():    
-    
-    x,y,dates = load_data()
+def create_test_train_set(x,y):
     
     test_sample_id = np.random.choice(range(len(x)), size=2000,replace=False)
     
@@ -60,6 +57,14 @@ def regresion_test():
     
     train_y = y.drop(test_sample_id, 0)    
     
+    return test_x, train_x, test_y, train_y
+    
+    
+def regression_test():    
+    
+    x,y,dates,movies = load_data()
+    
+    test_x, train_x, test_y, train_y = create_test_train_set(x, y)    
     
     '''
         Base line RMSE
@@ -78,22 +83,86 @@ def regresion_test():
     
     out_sample[out_sample>5] = 5
     
-    print "Clipped out sample RMSE:", np.sqrt(np.mean((test_y[0] - out_sample)**2))
+    print "Clipped out sample RMSE:", np.sqrt(np.mean((test_y[0] - out_sample)**2))    
     
+    test_x = test_x.iloc[:, range(14)]
     
-    test_x = x.iloc[test_sample_id,range(14)]
+    train_x = train_x.iloc[:, range(14)]
     
-    test_y = y.iloc[test_sample_id]
+    res = sm.OLS(train_y, train_x).fit() 
     
-    train_x = x.drop(test_sample_id, 0).iloc[:,range(14)]
-    
-    train_y = y.drop(test_sample_id, 0)
-    
-    res = sm.OLS(train_y, train_x).fit()   
+    print res.summary()
     
     out_sample = res.predict(test_x)
     
     print "Completed data (14 movies) RMSE:", np.sqrt(np.mean((test_y[0] - out_sample)**2))
+    
+    
+def pca_regression_test():
+    from sklearn.decomposition import PCA    
+    
+    import matplotlib.pyplot as plt    
+    
+    x,y,dates,movies = load_data()
+    
+    x = x - np.mean(x)       
+    
+    x = x.ix[:, range(14)]
+    
+    pca = PCA(n_components=3)
+    
+    fitted = pca.fit(x)
+    
+    loadings = pd.DataFrame({"Comp1":fitted.components_[0], 
+                             "Comp2":fitted.components_[1],
+                             "Comp3":fitted.components_[2] })
+    
+    loadings = loadings.set_index(movies.ix[:,1].values[0:14])
+    
+    print "Loadings:", loadings
+    
+    stdev = pd.Series(np.sqrt(fitted.explained_variance_), index=["Comp1", "Comp2", "Comp3"])
+    
+    print "stdev:", stdev
+    
+    transformed_x = fitted.fit_transform(x)
+    
+    f, ax = plt.subplots(2,2)
+    ax[0,0].set_title("Comp1 ~ Comp2")    
+    ax[0,0].scatter(transformed_x[range(100),0], transformed_x[range(100),1])
+    
+    ax[0,1].set_title("Comp2 ~ Comp3")
+    ax[0,1].scatter(transformed_x[range(100),1], transformed_x[range(100),2])
+    
+    ax[1,0].set_title("Comp1 ~ Comp3")
+    ax[1,0].scatter(transformed_x[range(100),0], transformed_x[range(100),2])     
+    
+    f, ax = plt.subplots(2,1)  
+    
+    ax[0].set_title("Most high Comp1 scores")
+    ax[0].hist(y.ix[transformed_x.argsort(0)[range(100),0]].values)
+    
+    
+    ax[1].set_title("Most low Comp1 scores")
+    ax[1].hist(y.ix[transformed_x.argsort(0)[::-1,0]][0:100].values)
+    
+    plt.tight_layout()
+    plt.show()    
+    
+    test_x, train_x, test_y, train_y = create_test_train_set(pd.DataFrame(transformed_x,columns=["c1","c2", "c3"]), y)    
+    
+    tr = train_x.ix[:, ["c1","c2"]]
+    tr["y"] = train_y
+    
+    res = sm.ols(formula="y~c1+c2",data=tr).fit()
+    
+    print res.summary()
+    
+    
+    
+    
+    
+    
     
     
     
