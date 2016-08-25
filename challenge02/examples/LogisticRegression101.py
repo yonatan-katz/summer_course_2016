844d06dbce2b1368fbed828ad930fcce4c358bb8
yonatan-katz <yonic2006@gmail.com>
2016-08-25 14:10:52 +0300

    Gradient boosting example

--- challenge02/examples/LogisticRegression101.py
+++ challenge02/examples/LogisticRegression101.py
@@ -9,11 +9,13 @@ import pandas as pd
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 import numpy as np
+import itertools
 import sklearn.ensemble
+import sklearn.feature_extraction
 import os
  
-work_dir = r'c:\Temp\Ex2'
-os.chdir(work_dir)
+the_dir = ''
+os.chdir(the_dir)
  
 train_df = pd.read_csv('./charity.train.csv.gz', compression='gzip')
 valid_df = pd.read_csv('./charity.valid.csv.gz', compression='gzip')
@@ -61,5 +63,28 @@ glm_fit = smf.glm(formula='y~'+cols, data=train_small_df, family=sm.families.Bin
 y_hat = glm_fit.predict(valid_small_df)
 print calculate_deviance(y_hat, valid_small_df.y)
 # 0.990012
-    
-    
+ 
+# Sklearn input is different - need to create X and y as numpy arrays
+# Also need to take care of categorical x
+enc = sklearn.feature_extraction.DictVectorizer()
+enc.fit([{'tmp':x} for x in train_small_df.RFA_3R.values])
+def create_sklearn_compatible_x_y(df):
+    x1 = df[['MAXRAMNT', 'RFA_3F', 'RFA_3A']].values
+    x2 = enc.transform([{'tmp':x} for x in df.RFA_3R.values]).toarray()
+    X = np.concatenate([x1, x2], axis=1)
+    y = df.y.values
+    return X, y
+X_train, y_train = create_sklearn_compatible_x_y(train_small_df)
+X_valid, y_valid = create_sklearn_compatible_x_y(valid_small_df)
+ 
+####### Step 3: Gradient boosting
+max_depth = 2
+learning_rate = .1
+n_estimators = 50
+ 
+model = sklearn.ensemble.GradientBoostingClassifier(loss='deviance',
+    learning_rate=learning_rate, n_estimators=n_estimators, max_depth=max_depth)
+model.fit(X_train, y_train)
+y_hat = model.predict_proba(X_valid)
+print calculate_deviance(y_hat[:, 1], valid_small_df.y)
+# 0.98908
