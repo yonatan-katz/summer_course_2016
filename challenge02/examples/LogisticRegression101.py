2e53706feffa5cfedc34e684a64ebf3c703c186e
yonatan-katz <yonic2006@gmail.com>
2016-08-25 13:07:06 +0300

    Add thermometer encoding

--- challenge02/examples/LogisticRegression101.py
+++ challenge02/examples/LogisticRegression101.py
@@ -9,24 +9,57 @@ import pandas as pd
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 import numpy as np
-
-if __name__ == '__main__':
-    import os
-    os.getcwd()
-    os.chdir(r'C:\share\everyone\SummerCourse\Public\charity')
-    train_df = pd.read_csv('./charity.train.csv.gz', compression='gzip')
-    valid_df = pd.read_csv('./charity.valid.csv.gz', compression='gzip')
-    train_small_df = train_df[['CHARITY_AMOUNT', 'RFA_2F', 'RFA_2A']]
-    train_small_df['y'] = (train_small_df['CHARITY_AMOUNT'] > 0).astype(int)
-    valid_df['y'] = (valid_df['CHARITY_AMOUNT'] > 0).astype(int)
-    #glm_fit = smf.glm(formula="y~RFA_2F+RFA_2A", data=train_small_df, family=sm.families.Binomial()).fit()
-    glm_fit = smf.glm(formula="y~RFA_2F+RFA_2A", data=train_small_df, family=sm.families.Binomial()).fit()
-    #glm_fit.summary()
-    y_hat = glm_fit.predict(valid_df)
-    null_p = np.mean(valid_df['y'].astype(int))
+import sklearn.ensemble
+import os
+ 
+work_dir = r'c:\Temp\Ex2'
+os.chdir(work_dir)
+ 
+train_df = pd.read_csv('./charity.train.csv.gz', compression='gzip')
+valid_df = pd.read_csv('./charity.valid.csv.gz', compression='gzip')
+ 
+def get_quantiles(x, num_levels):
+    quantiles = np.linspace(0, 1, num_levels)[1:]
+    return x.quantile(quantiles)
+ 
+def thermo_enc(df, col, levels):
+    for n, level in enumerate(levels):
+        df['{}_t{}'.format(col, n)] = df[col] <= level
+    return df
+ 
+quant = get_quantiles(train_df.MAXRAMNT, 5)
+def prepare_df(df):
+    df = df[['CHARITY_AMOUNT', 'MAXRAMNT', 'RFA_3']]
+    df['y'] = (df['CHARITY_AMOUNT'] > 0).astype(int)
+    df['RFA_3R'] = df.RFA_3.apply(lambda x: x[0] if len(x)>1 else 'X')
+    df['RFA_3F'] = df.RFA_3.apply(lambda x: int(x[1]) if len(x)>1 else -1)
+    df['RFA_3A'] = df.RFA_3.apply(lambda x: ord(x[2])-ord('A') if len(x)>1 else -1)
+    df = thermo_enc(df, 'MAXRAMNT', quant.values)
+    df = df.drop(['CHARITY_AMOUNT', 'RFA_3'], axis=1)
+    return df
+ 
+def calculate_deviance(y_hat, y):
+    null_p = np.mean(y.astype(int))
     null_deviance = -2*(null_p * np.log(null_p)+(1-null_p)*np.log(1-null_p))
-    log_loss = valid_df['y']*np.log(y_hat)+(1-valid_df['y'])*np.log(1-y_hat)
+    log_loss = y*np.log(y_hat)+(1-y)*np.log(1-y_hat)
     deviance = -2*np.mean(log_loss)
-    deviance/null_deviance
+    return deviance/null_deviance
+ 
+train_small_df = prepare_df(train_df)
+valid_small_df = prepare_df(valid_df)
+ 
+####### Step 1: Logistic regression without interactions
+glm_fit = smf.glm(formula='y~MAXRAMNT+RFA_3R+RFA_3F+RFA_3A',
+                  data=train_small_df, family=sm.families.Binomial()).fit()
+y_hat = glm_fit.predict(valid_small_df)
+print calculate_deviance(y_hat, valid_small_df.y)
+# 0.989715
+ 
+####### Step 2: Logistic regression with thermometer encoding
+cols = 'MAXRAMNT+MAXRAMNT_t0+MAXRAMNT_t1+MAXRAMNT_t2+MAXRAMNT_t3+RFA_3R+RFA_3F+RFA_3A'
+glm_fit = smf.glm(formula='y~'+cols, data=train_small_df, family=sm.families.Binomial()).fit()
+y_hat = glm_fit.predict(valid_small_df)
+print calculate_deviance(y_hat, valid_small_df.y)
+# 0.990012
     
     
